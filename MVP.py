429a7e4c8bfb17d5b951c0868e6aff15d84f1d96
Rob-Geurts <77699721+Rob-Geurts@users.noreply.github.com>
2023-05-31 13:53:06 +0200

    Add files via upload

---
+++ MVP.py
@@ -0,0 +1,191 @@
+import streamlit as st
+import pandas as pd
+from yahooquery import Ticker
+from datetime import datetime
+from docx import Document
+import base64
+from download_button_function import download_button
+import io
+from create_word_doc import create_doc
+import matplotlib.pyplot as plt
+import seaborn as sns
+import requests
+from numerize import numerize
+from PIL import Image
+
+# To run use streamlit run streamlit.py
+# to exit, use control-c
+st.set_page_config(layout="wide")
+
+st.sidebar.subheader("""DDR Edge Stock Analysis""")
+user_input_ticker = st.sidebar.text_input("Enter the ticker of the stock","AAPL")
+author = st.sidebar.text_input("Your name", "Rob Geurts")
+user_input_competitors = st.sidebar.text_input("Enter names/tickers of competitor stocks, separate stocks with a comma without space:", "MSFT,TSLA")  # Changed from Dutch to English
+# Date = current date
+date = datetime.today().date
+
+col1, col2, col3 = st.columns(3)
+
+
+
+
+
+def main():
+    col1.subheader('Company Information')
+    col2.subheader('Competition and Price Chart')
+    col3.subheader('Dividend Payouts')
+
+    data_1_laden = col1.text('Loading...')
+    ticker = user_input_ticker
+    blueprint = Ticker(user_input_ticker)
+
+    #company name
+    companyName = 'Name: ' + blueprint.quote_type[ticker]['longName']
+    shortcompanyName = blueprint.quote_type[ticker]['shortName']
+    # Sector
+    sector = 'Sector: ' + blueprint.asset_profile[ticker]['sector']
+    # Industry
+    industry = 'Industry: ' + blueprint.asset_profile[ticker]['industry']
+    # Current price
+    current_price = 'Current price: ' + str(round(blueprint.financial_data[ticker]['currentPrice'],2))
+    # 52 weeks high and low
+    fiftyTwoWeek = "52 weeks l/h: " + str(round(blueprint.summary_detail[ticker]['fiftyTwoWeekLow'],2)) + ' - ' + str(round(blueprint.summary_detail[ticker]['fiftyTwoWeekHigh'],2))
+    # 1 year target
+    targetMeanPrice = "1 Year Target: " + str(blueprint.financial_data[ticker]['targetMeanPrice'])
+    # Market cap
+    marketCap = "Market Cap: " + str(numerize.numerize(blueprint.summary_detail[ticker]['marketCap']))
+
+    if type(blueprint.summary_detail[ticker]['trailingAnnualDividendRate']) == type(None):
+        dividendRate = "Dividend N/A"
+    else:
+        dividendRate = "Forward Dividend: " + str(blueprint.summary_detail[ticker]['trailingAnnualDividendRate']) + " (" + str(
+            round(blueprint.summary_detail[ticker]['trailingAnnualDividendYield'] * 100, 2)) + "%)"
+        
+    # Beta
+    if type(blueprint.summary_detail[ticker]['beta']) == type(None):
+        beta = "Beta N/A"
+    else:
+        beta = "Beta: " + str(round(blueprint.summary_detail[ticker]['beta'], 3))
+
+    # Company info 
+    companyInfo = blueprint.asset_profile[ticker]['longBusinessSummary']  # company info
+
+
+    col1.write(companyName)
+    col1.write(sector)
+    col1.write(industry)
+    col1.write(current_price)
+    col1.write(fiftyTwoWeek)
+    col1.write(targetMeanPrice)
+    col1.write(marketCap)
+    col1.write(beta)
+    col1.write(dividendRate)
+    col1.write(companyInfo)
+    data_1_laden.text('Loading... ready')
+    data_2_laden = col2.text('Loading...')
+    user_list = [user_input_ticker]
+    competitorlist = user_input_competitors.split(",")
+    comparingList = user_list + competitorlist
+
+    if user_input_competitors == "":
+        comparingList = user_list
+
+    #Initialize DF for comparison
+    compare_df = pd.DataFrame(columns=[
+        "Company name", "Total D/E", "Current Ratio", "Trailing P/E",
+        "Return on Equity", "Profit Margin", "Trailing Annual Dividend Yield", "Enterprise value/EBITDA"
+        ])
+
+    for company in comparingList:
+        items = []
+        tickerb = company
+        blueprintb = Ticker(company)
+        items.append(blueprintb.quote_type[tickerb]['shortName'])
+        items.append(blueprintb.financial_data[tickerb]['debtToEquity'])
+        items.append(blueprintb.financial_data[tickerb]['currentRatio'])
+        items.append(blueprintb.summary_detail[tickerb]['trailingPE'])
+        items.append(blueprintb.financial_data[tickerb]['returnOnEquity'])
+        items.append(blueprintb.financial_data[tickerb]['profitMargins'])
+        items.append(blueprintb.summary_detail[tickerb]['trailingAnnualDividendYield'])
+        items.append(blueprintb.key_stats[tickerb]['enterpriseToEbitda'])
+        compare_df.loc[len(compare_df)] = items
+
+
+    # Graph of stock for 2 years ( year input by user)
+
+    ## Short positions
+    # Short Ratio
+    shortRatio = "Short Ratio: " + str(blueprint.key_stats[ticker]['shortRatio'])
+    # Short % of shares outstanding
+    shortPercentage = "Short % of Shares Outstanding: " + str(blueprint.key_stats[ticker]['shortPercentOfFloat'])
+
+    print(compare_df)
+    # News regarding company
+    #news = ticker.news
+    news = "no news"
+
+    col2.dataframe(compare_df)
+
+    # Graph
+    graph_data = blueprint.history(period = '2y',interval = '1d' )
+    col2.line_chart(graph_data.adjclose) #for on the webpage
+
+    data_2_laden.text('Loading... ready')
+
+    #graph to use in document
+    memfile = io.BytesIO()
+    sns.lineplot(data = graph_data.Close)
+    plt.xticks(rotation=30)
+    plt.savefig(memfile)
+
+
+
+    # Dividend history
+    data_3_laden = col3.text('Loading...')
+    dividend_df = blueprint.history(period='2y')
+    dividend_df = dividend_df[dividend_df['dividends']>0]
+    dividend_df.reset_index(inplace=True)
+    dividend_df.set_index(pd.to_datetime(dividend_df['date']), inplace=True, drop=True)
+    dividend_df = dividend_df[['dividends']]
+    dividend_df.index = dividend_df.index.strftime('%Y-%m-%d')
+    dividend_df = dividend_df.sort_values(by='date', ascending=False)
+    col3.dataframe(dividend_df)
+
+    col3.subheader("Short ratios")
+    col3.write(shortRatio)
+    col3.write(shortPercentage)
+
+
+    data_3_laden.text('loading... ready')
+    document = create_doc(companyName, sector, industry, current_price,
+               fiftyTwoWeek, targetMeanPrice,
+               marketCap, beta, dividendRate, companyInfo,
+               shortRatio, shortPercentage, dividend_df,
+               news, compare_df,memfile, author)
+
+    file_stream = io.BytesIO()
+    # Save the .docx to the buffer
+    document.save(file_stream)
+    # Reset the buffer's file-pointer to the beginning of the file
+    file_stream.seek(0)
+    # convert doc to b64
+    b64 = base64.b64encode(file_stream.getvalue()).decode()
+    filename = "Analysis " + shortcompanyName + ".docx"
+    download_button_str = download_button(b64, filename, f'Click here to download {filename}')
+    st.sidebar.markdown(download_button_str, unsafe_allow_html=True)
+
+
+if st.sidebar.button('GO'):
+    main()
+
+st.sidebar.write("Once the document is ready, a download link will appear here")
+#if st.button('test'):
+    #main()
+
+#main()
+
+#if __name__ == "__main__":
+   # main()
+
+
+
---
+++ create_word_doc.py
@@ -0,0 +1,93 @@
+import pandas as pd
+from docx import Document
+from datetime import datetime
+from docx.shared import Inches
+
+
+def find_replace(paragraph_keyword, draft_keyword, paragraph):
+    if paragraph_keyword in paragraph.text:
+        paragraph.text = paragraph.text.replace(paragraph_keyword, draft_keyword)
+
+
+def create_doc(companyName, sector, industry, current_price,
+                fiftyTwoWeek, targetMeanPrice,
+               marketCap, beta, dividendRate, companyInfo,
+               shortRatio, shortPercentage, dividendHistory,
+               news, competition_df,plot,author):
+    document = Document('Empty_koopvoorstel.docx')
+
+
+
+    # replace the text in paragraphs
+    for paragraph in document.paragraphs:
+        paragraphtext = paragraph.text
+        find_replace("[COMPANY_INFO]",companyInfo, paragraph)
+        find_replace("[Short Ratio:]", shortRatio, paragraph)
+        find_replace("[Short % of Shares Outstanding:]", shortPercentage, paragraph)
+
+    ## add logo
+    #logokeyword = "[Logo]"
+    #for paragraph in document.paragraphs:
+    #    if logokeyword in paragraph.text:
+    #        r = paragraph.add_run()
+    #        r.add_picture(imagelogo)
+
+
+    # replace the text in tables
+    for table in document.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                for paragraph in cell.paragraphs:
+                    find_replace("Company:", companyName, paragraph )
+                    find_replace("Sector:", sector, paragraph)
+                    find_replace("Industry:", industry, paragraph)
+                    find_replace("Current price:", current_price, paragraph)
+                    find_replace("52-week l/h:", fiftyTwoWeek, paragraph)
+                    find_replace("1-year target:", targetMeanPrice, paragraph)
+                    find_replace("Market cap:", marketCap, paragraph)
+                    find_replace("Beta:", beta, paragraph)
+                    find_replace("Forward Dividend:", dividendRate, paragraph)
+                    find_replace("Date:", "Date: " + str(datetime.today().date()), paragraph)
+                    find_replace("Name:", "Name: " + str(author), paragraph)
+
+    # input table for competitor
+    competitor_df = competition_df
+    table = document.add_table(competitor_df.shape[0]+1, competitor_df.shape[1])
+    for j in range(competitor_df.shape[-1]):
+        table.cell(0, j).text = competitor_df.columns[j]
+
+    for i in range(competitor_df.shape[0]):
+        for j in range(competitor_df.shape[-1]):
+            table.cell(i + 1, j).text = str(competitor_df.values[i, j])
+
+    # replace new table with old table
+    old_competitor_table = document.tables[2]
+    newTable = document.tables[4]
+    old_competitor_table._element.getparent().replace(old_competitor_table._element,newTable._element )
+
+    ## Dividends table
+
+    dividend_df = dividendHistory
+    dividend_df = dividend_df.reset_index()
+    table2 = document.add_table(dividend_df.shape[0] + 1, dividend_df.shape[1])
+    for j in range(dividend_df.shape[-1]):
+        table2.cell(0, j).text = dividend_df.columns[j]
+
+    for i in range(dividend_df.shape[0]):
+        for j in range(dividend_df.shape[-1]):
+            table2.cell(i + 1, j).text = str(dividend_df.values[i, j])
+    # replace new table with old table
+    old_dividend_table = document.tables[3]
+    newTable = document.tables[4]
+    old_dividend_table._element.getparent().replace(old_dividend_table._element, newTable._element)
+
+    ## add graph
+    keyword = "Price graph"
+    for paragraph in document.paragraphs:
+        if keyword in paragraph.text:
+            r = paragraph.add_run()
+            r.add_picture(plot)
+
+
+    return(document)
+
