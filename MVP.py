6bb0ec1d9dd89cece0cc7f14f8916d3c4608e091
Rob-Geurts <77699721+Rob-Geurts@users.noreply.github.com>
2023-05-31 14:15:43 +0200

    Update MVP.py

--- MVP.py
+++ MVP.py
@@ -128,8 +128,8 @@ def main():
 
     # Graph
     graph_data = blueprint.history(period = '2y',interval = '1d' )
-    graph_data['index'] = graph_data.index
-   
+    graph_data['index'] = pd.to_datetime(graph_data.index)
+
     col2.line_chart(graph_data, x='index', y='adjclose')
 
 
